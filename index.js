e85e208782aee3f1cdbf478c1f882f26ed90e448
Keith <keithwesley187@gmail.com>
2022-06-07 13:59:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(someAction = 'roller-skate'){
+    return (`This Saturday, I want to ${someAction}!`);
+}
+saturdayFun(someAction)
+
+
+function mondayWork(mondayAction= "go to the office"){
+    return(`This Monday, I will ${mondayAction}.`)
+}
+mondayWork(mondayAction)
+
+
+function wrapAdjective(characters= '*'){
+    return function (bombo= "special") {
+        return `You are ${characters}${bombo}${characters}!`;
+    }
+}
